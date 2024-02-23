f89628024df6353ca4478c06629301358e44311b
Teta_Lena <tetalenaa@gmail.com>
2024-02-23 18:45:12 +0200

    index.js set

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,11 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-const serverless = require("serverless-http");
 const router = require("./routes");
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-// module.exports.handler = serverless(app);
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
