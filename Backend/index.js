7a5edc1e98777e6d7de10e556f52ef473508e341
Teta_Lena <tetalenaa@gmail.com>
2024-02-23 21:34:07 +0200

    cors errors

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,12 @@
 const http = require("http");
 const app = require("./app");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specified HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specified headers
+  next();
+});
 const server = http.createServer(app);
 const router = require("./routes");
 
