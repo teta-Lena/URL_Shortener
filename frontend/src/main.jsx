55ef8eb6365e970fa2401f390fabc2b871520c03
Teta_Lena <tetalenaa@gmail.com>
2024-02-23 19:15:54 +0200

    deployed apps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import axios from 'axios'
-import { ToastContainer } from 'react-toastify'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-axios.defaults.baseURL="http://localhost:4000/";
-ReactDOM.createRoot(document.getElementById('root')).render(
+axios.defaults.baseURL = "https://tlxnaly-url-app.vercel.app/";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ToastContainer/>
+    <ToastContainer />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
